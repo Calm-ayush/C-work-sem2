c30264aeeccd43f2a45ecc0b304f28afa4157738
Calm-ayush <ayushrollno5@gmail.com>
2026-02-10 22:25:23 +0530

    Prime no

---
+++ class6/5.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <math.h>
+
+bool isPrime(int n) {
+    if (n <= 1) return false;
+    if (n == 2) return true;
+    if (n % 2 == 0) return false;
+    for (int i = 3; i <= sqrt(n); i += 2) {
+        if (n % i == 0) return false;
+    }
+    return true;
+}
+
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    if (isPrime(num))
+        printf("%d is prime\n", num);
+    else
+        printf("%d is not prime\n", num);
+    return 0;
+}
