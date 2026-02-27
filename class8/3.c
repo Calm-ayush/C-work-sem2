2bdc7c17153a0ae36a525afe54886809e0c94d5f
Calm-ayush <ayushrollno5@gmail.com>
2026-02-27 22:58:32 +0530

    Implement logic to find largest and second largest
    Add functionality to find the largest and second largest numbers in an array.

--- class8/3.c
+++ class8/3.c
@@ -1,1 +1,44 @@
+#include <stdio.h>
 
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    if (n < 2)
+        return 0;
+
+    int a1[n];
+
+    for (int i = 0; i < n; i++)
+        scanf("%d", &a1[i]);
+
+    int a = a1[0];
+    int b = a1[0];
+
+    for (int i = 1; i < n; i++)
+        if (a1[i] > a)
+            a = a1[i];
+
+    int f = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (a1[i] < a)
+        {
+            if (!f || a1[i] > b)
+            {
+                b = a1[i];
+                f = 1;
+            }
+        }
+    }
+
+    if (f)
+    {
+        printf("%d\n", a);
+        printf("%d\n", b);
+    }
+
+    return 0;
+}/
