422cf601ea7bc4fc947c66d605dfad350131eaf2
Calm-ayush <ayushrollno5@gmail.com>
2026-05-06 22:25:49 +0530

    new

---
+++ class20/q.c
@@ -0,0 +1,7 @@
+//1) wap to print a reverse of a number and sum of its digits;
+//2) wap to enter a decimal n too calculate and display the octal equivalant of this number;
+//3) wap to enter a hexadecimal no calc and display the decimal equiv of this number;
+//4) wap to find a given no is armstrong no or not;
+//5) wap to find the second largest using array of n numbers
+//6) wap to find if array of integers contains duplicate number or not 
+//7) wap to insert a number in array that is already sorted in ascending order
