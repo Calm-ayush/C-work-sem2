534d2fc113e64e76e53afaae051d3b0892cf80fb
Calm-ayush <ayushrollno5@gmail.com>
2026-05-13 00:50:51 +0530

    new

---
+++ class19/q.c
@@ -0,0 +1,9 @@
+//Write a program using an array of pointers to a structure to read and display the 
+//data of student (roll no, name, course, fees)
+//------------------------------
+//Write a program to read, display, add and subtract two heights, heights should be given
+// in feet and inches
+//---------------------------------
+// WAP to check whether a given string is palindrome or not
+//------------------------------
+//WAP to replace a pattern with another pattern in a given string
