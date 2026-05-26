8fce05e9493ec5d23f832312203ee21998056d24
Calm-ayush <ayushrollno5@gmail.com>
2026-05-26 22:57:52 +0530

    new

---
+++ class22/4.c
@@ -0,0 +1,25 @@
+
+/* Set the kth bit (0-based indexing) of n
+   and return the updated number */
+
+#include <stdio.h>
+int setKthBit(int n, int k)
+{
+    int m=1<<k;
+    return m|n;
+}
+int main()
+{
+    int t;
+    scanf("%d",&t);
+
+    while(t--)
+    {
+        int n,k;
+        scanf("%d %d",&n,&k);
+
+        printf("%d\n", setKthBit(n,k));
+    }
+
+    return 0;
+}
