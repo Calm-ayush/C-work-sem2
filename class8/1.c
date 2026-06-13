eff13c546ad2ba6c7de4deef97a1eb3796c7dc41
Calm-ayush <ayushrollno5@gmail.com>
2026-06-13 23:47:29 +0530

    new

--- class8/1.c
+++ class8/1.c
@@ -4,7 +4,7 @@ int main(){
     float a,b,c;
     printf("enter 2 numbers");
     scanf("%f %f %f",&a,&b,&c);
-    float form= (pow(a,2)+pow(b,2))-(2*a*b)+(c/(a-b));
-    printf("The equation is %.2f",form);
+    float f1= (pow(a,2)+pow(b,2))-(2*a*b)+(c/(a-b));
+    printf("The equation is %.2f",f1);
     return 0;
 }
